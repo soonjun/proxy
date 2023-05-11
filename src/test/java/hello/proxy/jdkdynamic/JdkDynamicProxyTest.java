8260e08ca10829041d1026117857eda72a4489a1
Sunjun Ok <remedime0@gmail.com>
2023-05-11 16:55:09 +0900

    동적 프록시 기술

---
+++ src/test/java/hello/proxy/jdkdynamic/JdkDynamicProxyTest.java
@@ -0,0 +1,35 @@
+package hello.proxy.jdkdynamic;
+
+import hello.proxy.jdkdynamic.code.*;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Proxy;
+
+@Slf4j
+public class JdkDynamicProxyTest {
+
+    @Test
+    void dynamicA() {
+        AImpl target = new AImpl();
+        TimeinvocationHandler handler = new TimeinvocationHandler(target);
+
+        AInterface proxy = (AInterface) Proxy.newProxyInstance(AInterface.class.getClassLoader(), new Class[]{AInterface.class}, handler);
+
+        proxy.call();
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+    }
+
+    @Test
+    void dynamicB() {
+        BImpl target = new BImpl();
+        TimeinvocationHandler handler = new TimeinvocationHandler(target);
+
+        BInterface proxy = (BInterface) Proxy.newProxyInstance(BInterface.class.getClassLoader(), new Class[]{BInterface.class}, handler);
+
+        proxy.call();
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+    }
+}
